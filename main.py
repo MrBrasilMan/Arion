dc1351b9bfc9daad8588c11f9ecd9a2c4417aee7
MrBrasilMan <76192385+MrBrasilMan@users.noreply.github.com>
2021-05-20 10:34:22 -0500

    Updating main.py for 1.5.0
    Added right click with copy and paste menu, new icon on top left hand side, new <i>super hidden</i> Easter Egg, and a few crash fixes!

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import tkinter.messagebox as msgbox
 import parser
 import sys
 import requests
+
+import tkinter as tk
+from tkinter import ttk
+
+
 #Warning, this is some pretty messy code right now
 #Preset webtext to the home page
-version = "1.0.0"
+version = "1.5.0"
 latest_info = requests.get("https://raw.github.com/MrBrasilMan/Arion/main/info.txt").text
 webtext = "Welcome to Arion Version " + version + "\nBy Lucas Frias\n___________________________________________________\n" + latest_info
 #This is the web library of 0.4.2, somewhat modified.
@@ -23,7 +28,7 @@ def get_website():
        body = requests.get("https://" + search.get(1.0, "end-1c"))
     #This places the parsed html in the website text, plus an end of page, and list of links belowards.
     window.title(parser.get_title(search.get(1.0, "end-1c")) + " - Arion")
-    website_text = parser.parse(body.text) + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n__________________________________\nList of links (elements of a)\n__________________________________\n" + parser.link_list(body.text)
+    website_text = parser.parse(body.text) + "\n\n\n\n__________________________________\nList of links (elements of a)\n__________________________________\n" + parser.link_list(body.text)
     #Delete all of the text from the previous website
     website_body_text.delete('1.0', END)
     #This is putting the text in the main textbox to view.
@@ -36,23 +41,29 @@ def get_website():
           a = body
           website_body_text.delete('1.0', END)
           if body.status_code != 200:
-            website_body_text.insert(tk.END, "Error\nArion was able to establish a connection to the website.\n When talking to the website however, it sent a " + str(body.status_code) + " response\nMake sure the given website is not forbidden or broken")
+                  website_body_text.insert(tk.END, "Error\nArion was able to establish a connection to the website.\n When talking to the website however, it sent a " + str(body.status_code) + " response\nMake sure the given website is not forbidden or broken")
           if body.status_code == 200:
               #If it sends a two hundred, but an error occurs, it is almost most likely that there is a software error.
-            website_body_text.insert(tk.END, "Error\n A software error occured when trying to load this website.\nArion is trying it's hardest to improve. Any issues or feedback is appriceated.")
+            website_body_text.insert(tk.END, "Error\n A software error occured when trying to load this website.\nArion is trying its hardest to improve. Any issues or feedback is appriceated.")
 
 
       except:
           #No internet response
-        website_body_text.delete('1.0', END)
-        website_body_text.insert(tk.END, "Error\nWhen connecting to the website, we got no response\n\nTrying checking the name and make sure you did not mispell something. You can use the full service name (https://www.example.com) and try again\n\nIf this problem persits, check your network firewall")
+        if search.get(1.0, "end-1c") == "":
+            website_body_text.delete('1.0', END)
+            window.title("What??????? - Arion")
+            website_body_text.insert(tk.END, "Unidentified Bored Pigeon Error\n\nAs all people know, poultery is what powers today's modern computers. \nThe CPU (Chicken Processing Unit) is the main power of your computer.\nThe main problem with today's modern computing is, unlike back then when code was written in chicken friendly Assembler, we write very complex programs.\nThe chicken was already stressed out enough running this and god knows how many other programs.\nSo when the chicken saw it did all of that work just to calculate nothing, it just gave up.\n\nI hope you're happy now.\n\nProgrammers are also legally required to have at least one weird easter egg in their code.")
+        else:
+            website_body_text.delete('1.0', END)
+            website_body_text.insert(tk.END, "Error\nWhen connecting to the website, we got no response\n\nTrying checking the name and make sure you did not mispell something. You can use the full service name (https://www.example.com) and try again\n\nIf this problem persits, check your network firewall")
 
 #Starting out dimension
 window = Tk()
-window.resizable(False, False)
+#window.resizable(False, False)
 window.title("Home - Arion")
 #Set icon
 photo = PhotoImage(file = "horse.png")
+icon = PhotoImage(file = "horse_small.png")
 window.iconphoto(False, photo)
 window.geometry('700x420')
 #Centering code shamelessly and gleefully stolen from
@@ -76,8 +87,10 @@ gobutton = Button(window,
   activebackground="#7ddf3f",
   activeforeground="white",
 )
-#canvas = Canvas(window, width=600, height=600)
-#canvas.create_image(1,1, image=photo)
+canvas = Canvas(window, width=40, height=40, borderwidth=0)
+canvas.create_image(1,1, image=icon)
+
+
 exitbutton = Button(window,
                      text="Exit",
                      command=sys.exit,
@@ -97,13 +110,48 @@ website_body_text = ScrolledText(
   borderwidth=0,
   highlightthickness=0,
   )
+def copy():
+    try:
+        window.clipboard_append(website_body_text.selection_get())
+    except:
+        window.clipboard_append("")
+m = Menu(window, tearoff=0)
+m.add_command(label="Copy", command=copy)
+m.add_command(label="Reload", command=get_website)
+m.add_separator()
+m.add_command(label="Pass")
+def do_popup(event):
+    try:
+        m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+website_body_text.bind("<Button-3>", do_popup)
+
+def paste():
+    try:
+        print(window.clipboard_get())
+        search.insert(tk.END, str(window.clipboard_get()))
+    except:
+        window.clipboard_append("")
+n = Menu(window, tearoff=0)
+n.add_command(label="Paste", command=paste)
+n.add_separator()
+n.add_command(label="Pass")
+def do_popupn(event):
+    try:
+        n.tk_popup(event.x_root, event.y_root)
+    finally:
+        pass
+        #n.grab_release()
+search.bind("<Button-3>", do_popupn)
+
 #Pack all parts of the application
 #gobutton.grid(column=1, row=0)
 #exitbutton.grid(column=1, row=1)
 #search.grid(column=0, row=0)
 #website_body_text.grid(column=0, row=7)
 gobutton.place(x=490, y=0)
-#canvas.place(x=10, y=10)
+canvas.place(x=10, y=10)
 search.place(x=120, y=5)
 website_body_text.place(x=0, y=50)
 website_body_text.insert(tk.END, webtext)
